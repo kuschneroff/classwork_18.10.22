fa8418fdf3d5f2b0e98935f6c86b984e9f21b9f3
Aleksey Kushnerov <kuschneroff.alex@yandex.ru>
2022-10-18 22:56:58 +0300

    Задача 42: Напишите программу, которая будет преобразовывать десятичное число в двоичное.

---
+++ Task_#3/Program.cs
@@ -0,0 +1,38 @@
+﻿/*Задача 42: Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+45 -> 101101
+3  -> 11
+2  -> 10
+*/
+
+ int GetNumber (string message)
+ {
+    Console.WriteLine(message);
+    int result = int.Parse(Console.ReadLine() ??"");
+    return result;
+
+ }
+
+ int[] DoubleSystem(int number)
+ {
+    List<int> array = new List<int>();
+    while (number >=1)
+    {
+        
+        array.Add(number%2);
+        number = number/2;
+
+    }
+    return array.ToArray();
+ }
+
+ void PrintArray(int[] arr)
+ {
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write(arr[i]);
+    }
+ }
+
+ int number = GetNumber("Введите число ");
+ int[] array = DoubleSystem(number);
+ PrintArray(array);
